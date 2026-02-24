6711e9b61454067952c13f24edd091c6b420b4a7
Teja <sanjeev@kodekloud.com>
2026-02-24 15:15:21 +0800

    Created SSMrole for EC2

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -20,9 +20,39 @@ resource "aws_security_group" "app_sg" {
        }
 }
 
+# Create the IAM Role
+
+resource "aws_iam_role" "ec2_ssm_role" {
+    name = "app_server-ssm-role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            }
+        }]
+    })
+}
+
+# Attach the Managed Policy
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
+    role =  aws_iam_role.ec2_ssm_role.name 
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create the Instance Profile
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+    name = "app-server-ssm-profile"
+    role = aws_iam_role.ec2_ssm_role.name 
+}
 resource "aws_instance" "app_server" {
     ami = "ami-060e277c0d4cce553"
     instance_type = "t3.micro"
+    iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name 
     subnet_id = var.subnet_id
     vpc_security_group_ids = [aws_security_group.app_sg.id]
     tags = { Name = var.instance_name }
