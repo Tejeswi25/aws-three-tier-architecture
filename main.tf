41987d83f127963a9293e2ad4114a9871b30714a
Teja <sanjeev@kodekloud.com>
2026-02-24 16:56:55 +0800

    Enabled dns hostnames for SSMagent

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "hub_vpc" {
 module "app_vpc" {
   source              = "./modules/vpc"
   vpc_name            = "SG-App-VPC"
+  
   vpc_cidr            = "10.0.0.0/16"
   public_subnet_cidr  = "10.0.1.0/24"
   private_subnet_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -19,7 +19,9 @@ variable "azs" {
 
 resource "aws_vpc" "VPC" {
   cidr_block = var.vpc_cidr
-  enable_dns_hostnames = true 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  
   tags = { Name = var.vpc_name}
 }
 
